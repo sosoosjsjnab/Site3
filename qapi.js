1916abdfe9015b2d096df51a75c11a48ca6ce23c
sosoosjsjnab <74925201+sosoosjsjnab@users.noreply.github.com>
2020-11-24 21:58:57 +0300

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,49 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fbd547eb83a2f001f2e3098",
+				"5fbd547eb83a2f001f2e309a",
+				"5fbd577d2c4ef2001e32a1fa",
+				"5fbd57aacae167001e06dfbc",
+				"5fbd57c208b41a001fec7289"
+			]
+		},
+		"5fbd547eb83a2f001f2e3098": {
+			"id": "5fbd547eb83a2f001f2e3098",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fbd547eb83a2f001f2e309a": {
+			"id": "5fbd547eb83a2f001f2e309a",
+			"name": "index",
+			"pageUrl": "index",
+			"seo": {
+				"favicon_32px": "https://uploads.quarkly.io/5fbd547eb83a2f001f2e3095/images/a24128b7c77d9ea74f31c83d5c9527ef.jpg?v=2020-11-24T18:50:44.518Z"
+			}
+		},
+		"5fbd577d2c4ef2001e32a1fa": {
+			"id": "5fbd577d2c4ef2001e32a1fa",
+			"pageUrl": "terms",
+			"name": "Terms And Conditions",
+			"seo": {
+				"favicon_32px": "https://uploads.quarkly.io/5fbd547eb83a2f001f2e3095/images/a24128b7c77d9ea74f31c83d5c9527ef.jpg?v=2020-11-24T18:50:44.518Z"
+			}
+		},
+		"5fbd57aacae167001e06dfbc": {
+			"id": "5fbd57aacae167001e06dfbc",
+			"pageUrl": "privacy",
+			"name": "Privacy Policy"
+		},
+		"5fbd57c208b41a001fec7289": {
+			"id": "5fbd57c208b41a001fec7289",
+			"pageUrl": "cookies",
+			"name": "Cookies"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
---
+++ src/pages/index.js
@@ -0,0 +1,257 @@
+import React from "react";
+import theme from "theme";
+import { Theme, Link, Text, Box, Image, Input, Button } from "@quarkly/widgets";
+import { Helmet } from "react-helmet";
+import { StackItem, Stack, Section, Override, Formspree } from "@quarkly/components";
+export default (() => {
+	return <Theme theme={theme}>
+		<Helmet>
+			<title>
+				Quarkly export
+			</title>
+			<meta name={"description"} content={"Web site created using quarkly.io"} />
+			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/5fbd547eb83a2f001f2e3095/images/a24128b7c77d9ea74f31c83d5c9527ef.jpg?v=2020-11-24T18:50:44.518Z"} type={"image/x-icon"} />
+		</Helmet>
+		<Section
+			padding="140px 0"
+			sm-padding="40px 0"
+			background="linear-gradient(0deg,rgba(0, 0, 0, 0.88) 0%,rgba(0, 0, 0, 0.76) 100%),--color-dark url(https://uploads.quarkly.io/5fbd547eb83a2f001f2e3095/images/kak-krepit-profil-k-stene2.jpg?v=2020-11-24T18:50:45.028Z) 50% 15%/cover"
+			color="--light"
+			font="--base"
+		>
+			<Box>
+				<Text
+					as="h1"
+					margin="0 0 96px 0"
+					text-align="center"
+					letter-spacing="15px"
+					text-transform="uppercase"
+					font="100 50px/1.2 --fontFamily-sans"
+					lg-font="200 42px/1.2 --fontFamily-sans"
+					lg-letter-spacing="20px"
+				>
+					How to fix drywall: step by step instructions
+				</Text>
+			</Box>
+			<Stack>
+				<StackItem
+					width="25%"
+					lg-width="50%"
+					sm-width="100%"
+					font="--lead"
+					text-transform="uppercase"
+					letter-spacing="1px"
+				>
+					<Text margin="0px">
+						INNER PAGES
+					</Text>
+				</StackItem>
+				<StackItem width="25%" lg-width="50%" sm-width="100%">
+					<Text margin="0px">
+						<Link href="/terms">
+							Terms And Conditions
+						</Link>
+					</Text>
+				</StackItem>
+				<StackItem width="25%" lg-width="50%" sm-width="100%">
+					<Text margin="0px">
+						<Link href="/privacy">
+							Privacy Policy
+						</Link>
+					</Text>
+				</StackItem>
+				<StackItem width="25%" lg-width="50%" sm-width="100%">
+					<Text margin="0px">
+						<Link href="/cookies">
+							Cookies
+						</Link>
+					</Text>
+				</StackItem>
+			</Stack>
+		</Section>
+		<Section
+			padding="60px 0"
+			sm-padding="40px 0"
+			background="--color-light"
+			display="flex"
+			flex-direction="column"
+		>
+			<Stack color="--grey" font="--base" flex-wrap="wrap" align-items="center">
+				<StackItem md-width="100%" width="600px">
+					<Override slot="StackItemContent" display="block" width="1200px" />
+					<Text margin="0px" max-width="1160px" width="1160px">
+						Plasterboard is a building material that is used by many for its positive qualities. The result is a flat surface. To make the structure durable, you should pay attention to the fastening rules. If the actions are well thought out, then everyone can cope with the task without experience. Installation will be easier if there is a good assistant nearby. Preparation How to properly fix drywall? The first thing to think about is the required tool. The simplest and most affordable method is the use of aluminum profiles. The work will require: Cutting the profile with scissors. Construction level. Perforator. Screwdriver. Roulette or cord. Knife. Putty knife.
+					</Text>
+				</StackItem>
+				<StackItem md-width="100%" />
+			</Stack>
+		</Section>
+		<Section
+			background="linear-gradient(0deg,rgba(4, 8, 12, 0.6) 0%,rgba(4, 8, 12, 0.6) 100%),--color-darkL2 url(https://uploads.quarkly.io/5fbd547eb83a2f001f2e3095/images/fico-f3ba3d13.jpg?v=2020-11-24T18:50:44.556Z) center/cover"
+			padding="64px 0"
+			sm-padding="40px 0"
+			color="--light"
+			font="--base"
+		>
+			<Stack>
+				<StackItem width="75%" lg-width="100%">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Text color="--lightD2" letter-spacing="1px" text-transform="uppercase" margin="0">
+						Materials After purchasing material for work: Profiles and suspensions. Guides, bearing and transverse. Dowels. Self-tapping screws. Drywall sheets. Putty and mounting mesh. Once the entire set is ready, the main work begins. No need to work in a high humidity and low temperature room. After purchase, the best storage option is strictly in a horizontal position. Any construction work has its own algorithm. Don't break it. Stages of work How to fix drywall to the wall with your own hands? You shouldn't rush and miss moments. The sequence must be followed: Purchase of material. Layout and preparation. Installation of a frame structure from profiles. Fastening works.
+					</Text>
+				</StackItem>
+			</Stack>
+			<Box text-align="center" margin="96px 0 0 0" />
+		</Section>
+		<Section padding="100px 0" sm-padding="40px 0">
+			<Override slot="SectionContent" align-items="center" />
+			<Text as="p" font="--lead" margin="20px 0 0 0">
+				Getting Started The first thing you need to do is get rid of the dust. If this is a ceiling, then you will have to move to the walls. How to do it: There is the lowest point on the site. The building level is applied. Then the distance of the profile is measured to understand where the working surface is. Using a cord for measuring, a mark is made. A guide profile is fixed on the upper line, a screw is applied. The bottom edge matches the line on the wall. Afterwards, in the direction of the plasterboard sheets, marks are made after about 50 centimeters. The suspensions are located at this place. When measuring the profile, it is better to cut it off shorter so that it fits into the guide. In order not to be mistaken, a thread is pulled - a landmark goes along it. Further, the structure is leveled and fixed. The protruding ends of the suspensions are bent. Longitudinal profiles will not have the required rigidity. They will need to be fixed with transverse segments. If you need to install wires and other communications, then they are laid at this stage. Only then are the drywall sheets fixed. All components must match and fit together. Can you fix drywall with self-tapping screws? Yes, this is a convenient and affordable method. What self-tapping screws to fix drywall? Their hats should be buried, but not tear the surface. If a screwdriver is used in the work, then it must be with a limiter attachment (this way we will not damage our surface). When working with the ceiling, do not forget about lighting devices. Finishing with such material has its own characteristics. This is the formation of seams. They are hidden before finishing. Putty is used. If there are large holes, a mounting mesh will help. When the seams are closed, work can be done on the rest of the surface. How to fix drywall? It was a step-by-step work of elementary finishing. It is suitable for the ceiling. When everything is clear at this level, you can proceed to complex editing.
+			</Text>
+		</Section>
+		<Section padding="100px 0" sm-padding="40px 0">
+			<Override slot="SectionContent" align-items="center" />
+			<Image width="800px" height="800px" src="https://uploads.quarkly.io/5fbd547eb83a2f001f2e3095/images/kreplenie-stoek-k-stene-pryamim-podvesom.jpg?v=2020-11-24T18:50:44.548Z" />
+		</Section>
+		<Section
+			background="linear-gradient(0deg,rgba(4, 8, 12, 0.6) 0%,rgba(4, 8, 12, 0.6) 100%),--color-darkL2 url(https://uploads.quarkly.io/5fbd547eb83a2f001f2e3095/images/60085532_w640_h640_otdelka-sten.jpg?v=2020-11-24T18:50:44.523Z) center/cover"
+			padding="64px 0"
+			sm-padding="40px 0"
+			color="--light"
+			font="--base"
+		>
+			<Stack>
+				<StackItem width="75%" lg-width="100%">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Text color="--lightD2" letter-spacing="1px" text-transform="uppercase" margin="0">
+						How else is finishing done? The use of aluminum profiles is not the only method. Wooden lathing and glue masses are often used. But there are problems with the tree. One of them is exposure to moisture and temperature extremes. The glue will be appropriate if the finishing surface is flat, and if possible, you can strengthen the structure. The advantage of the glue is that there is practically no loss of space.{" \n\n"}
+					</Text>
+				</StackItem>
+			</Stack>
+			<Box text-align="center" margin="96px 0 0 0" />
+		</Section>
+		<Section
+			padding="60px 0"
+			sm-padding="40px 0"
+			background="--color-light"
+			display="flex"
+			flex-direction="column"
+		>
+			<Stack color="--grey" font="--base">
+				<StackItem width="25%" md-width="50%" sm-width="100%" />
+				<StackItem width="25%" md-width="50%" sm-width="100%">
+					<Text margin="0px">
+						It is worth completing several steps, and the drywall fixing without profiles will be completed: The surface is treated with a primer. An adhesive mass is applied to the sheet itself, after 40 centimeters, up and down. When the glue dries, it is worth reinforcing the structure with plastic dowels. Only after that the surface is putty. This method is the most convenient and does not require extra time for frame installation. Although the builders talk about the disadvantages. This is the impossibility of dismantling. These methods are suitable for finishing the ceiling, but sheets are also applicable to walls. Such material is considered universal, because it is suitable for a bathroom and a hall, as well as a loggia. It remains only to choose the appropriate mounting method. Frameless wall decoration with gypsum plasterboard sheets Today, there is an ordinary gypsum glue on sale, which allows the sheets to be attached to the wall without using additional structures. Before the plasterboard finish appears on the wall, you need to do the following: Level the surface as much as possible. If the finish is in the bathroom, then exclude shedding of the plaster and the presence of fungus. Eliminate the formation of condensation and freezing. Before starting the installation, it is worth preparing: Drywall. Depending on the room, a moisture resistant one is used. Plaster mix. Glue application tools and dilution containers. A sharp knife. Rule. Putty and tools for its application. Roulette and pencil. Building level and twine. Initially, the walls are marked. In this case, cruciform fixation is not allowed. The procedure is considered to be comparable to working on a ceiling. The surface is also initially prepared so that the grip is as reliable as possible. Any movable old finish is removed, otherwise everything will come off. The glue base should be applied using a grooved trowel. Now it is clear how to fix drywall to a wall without profiles.
+					</Text>
+				</StackItem>
+				<StackItem width="25%" md-width="50%" sm-width="100%">
+					<Text margin="0px">
+						Important aspects of the work According to the builders, the use of a frame is the best solution, since it turns out to level the surface. After the sheets are attached, the wall becomes smooth and even for finishing using available methods. In each work, the set of tools is standard, but the sheets need to be well calculated so as not to get too much. Depending on their size, the required amount is taken for the treated wall area. To avoid downtime in the work, it is better to immediately mark the wall. The wall becomes the beginning so that the frame does not go beyond. There is a building level in the work. The lines must be precise and straight, otherwise the work will not work. Today some people use a timber frame, but its service life is less than an aluminum one. A guide profile must be purchased. In work, do not forget about the sealing gum. The guide element is fixed to the ceiling. This is done carefully, without haste, taking into account the marking lines. In addition, with the help of self-tapping screws, the rack profile is attached to the ceiling and floor. The step for fasteners is half a meter or a little less. What is done next: When the frame is completed, a heater is placed under it, wiring is laid. Insulation is also necessary for sound absorption. It is better to choose it from materials that are not subject to combustion. Today, mineral wool is considered the sales leader in the market, because it meets all the requirements. When the necessary work is completed, it comes to fastening the sheets themselves. The required size is measured and cut off. For this, a joiner's knife is used. The edge is cleaned taking into account the standard direction. Each trim element is tested and adjusted if necessary. It should be mounted so that all joints fall on the guide profiles. Fasteners are recessed, but not deep. Then you need to carefully fix the coating. Holes are carefully cut out at the locations of sockets and bulbs. The butt channels and the location of the caps of the self-tapping screws are covered with putty compounds. That's all there is to it. After that, any decorative coating is already applied. But do not forget that a primer is used if necessary. In order for the joints to fill up beautifully and quickly, you need to use construction tape. It helps to avoid further cracking. There are various tapes on the market, it remains to choose the right one for your repair.
+					</Text>
+				</StackItem>
+				<StackItem width="25%" md-width="50%" sm-width="100%">
+					<Text margin="0px">
+						What shouldn't be forgotten? Sometimes, without construction experience, you can take some information for the truth, as a result, such a finish does not meet the requirements. Dismantling is not always convenient, for example, after applying glue. In order to prevent anything like this, before starting it is worth understanding the details and trifles. It's a good idea to watch videos and read tips from knowledgeable people. Some people believe that the best way is to use adhesives. Of course, this method saves time, since there is no need to mount the frame. But it is suitable for those walls and ceilings, the surface of which is flawlessly even. And this is rare. Sometimes it’s better to spend a little more time getting a finish that can last for years than just ruining the material and wasting time. Where can you apply? Today, many at the construction level are asking questions about which way of decorating walls and ceilings is preferable. To create a perfectly flat surface, there is no better material drywall. It is used in any premises: Houses and apartments (including bathrooms). Summer cottages. Offices. In the process of work, everyone will see a number of positive qualities. The main ones are: The ability to finish without experience. Obtaining a perfectly flat surface. The ability to use any decorative coating.
+					</Text>
+				</StackItem>
+			</Stack>
+		</Section>
+		<Section padding="100px 0" sm-padding="40px 0">
+			<Override slot="SectionContent" align-items="center" />
+			<Image width="800px" height="800px" src="https://uploads.quarkly.io/5fbd547eb83a2f001f2e3095/images/Foto-3-Stoechnye-profili-dolzhny-byt-ustanovleny-strogo-vertikalno.jpg?v=2020-11-24T18:50:44.523Z" />
+		</Section>
+		<Section
+			padding="60px 0"
+			sm-padding="40px 0"
+			min-height="600px"
+			sm-min-height="auto"
+			background="--color-light"
+			display="flex"
+			flex-direction="column"
+		>
+			<Stack margin-top="auto" color="--grey" font="--base">
+				<StackItem width="50%" md-width="100%">
+					<Text margin="0px">
+						Fixation When the choice is made, it is at the discretion of the decision on the method of fixation. The easiest and most convenient method is to use a special glue. Although mounting an additional frame will help in removing any surface imperfections. The only drawback in the case of a large skew of a wall or ceiling is a decrease in space. Warming or soundproofing material is also well included under drywall. Therefore, materials for this purpose are used in regions with any climate. Conclusion So, we found out how the installation of this material is carried out on different types of surfaces in the room. Drawing conclusions, it is easy to understand that drywall sheets are the best solution in decorating a room. There are not many questions that arise along the way. Now you know how to attach drywall to a ceiling or floor. This operation can be done by hand.
+					</Text>
+				</StackItem>
+				<StackItem width="50%" md-width="100%">
+					<Image width="400px" height="400px" src="https://uploads.quarkly.io/5fbd547eb83a2f001f2e3095/images/scale_1200.jpg?v=2020-11-24T18:50:44.532Z" />
+				</StackItem>
+			</Stack>
+		</Section>
+		<Section background="--color-light" color="--dark" padding="64px 0">
+			<Stack>
+				<StackItem width="50%" lg-width="100%">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Text
+						font="--base"
+						color="--grey"
+						letter-spacing="1px"
+						text-transform="uppercase"
+						margin="0"
+					>
+						Contact
+					</Text>
+					<Text font="--headline2" max-width="500px" margin="10px 0 0 0">
+						Email us, we would love to hear form you
+					</Text>
+				</StackItem>
+				<StackItem width="50%" lg-width="100%">
+					<Formspree>
+						<Stack gap="16px">
+							<StackItem width="50%">
+								<Override slot="StackItemContent" flex-direction="column" />
+								<Text font="--base" margin="0 0 4px 0">
+									Name
+								</Text>
+								<Input max-width="400px" width="100%" name="name" />
+							</StackItem>
+							<StackItem width="50%">
+								<Override slot="StackItemContent" flex-direction="column" />
+								<Text font="--base" margin="0 0 4px 0">
+									Email
+								</Text>
+								<Input max-width="400px" width="100%" type="email" name="email" />
+							</StackItem>
+							<StackItem width="100%">
+								<Override slot="StackItemContent" flex-direction="column" />
+								<Text font="--base" margin="0 0 4px 0">
+									Message
+								</Text>
+								<Input as="textarea" rows="4" width="100%" name="message" />
+							</StackItem>
+							<StackItem width="100%">
+								<Button>
+									Send
+								</Button>
+							</StackItem>
+						</Stack>
+					</Formspree>
+				</StackItem>
+			</Stack>
+		</Section>
+		<Link
+			font={"--capture"}
+			font-size={"10px"}
+			position={"fixed"}
+			bottom={"12px"}
+			right={"12px"}
+			z-index={"4"}
+			border-radius={"4px"}
+			padding={"5px 12px 4px"}
+			background-color={"--dark"}
+			opacity={"0.6"}
+			hover-opacity={"1"}
+			color={"--light"}
+			cursor={"pointer"}
+			transition={"--opacityOut"}
+			quarkly-title={"Badge"}
+			text-decoration-line={"initial"}
+			href={"https://quarkly.io/"}
+			target={"_blank"}
+		>
+			Made on Quarkly
+		</Link>
+	</Theme>;
+});
